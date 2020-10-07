0d8a0b2fcbd6fa909e33765ab006d858f60be1e6
LiliMR <71848241+LiliMR-lab@users.noreply.github.com>
2020-10-07 22:43:49 +0800

    Update tentukan-nilai.php

--- tentukan-nilai.php
+++ tentukan-nilai.php
@@ -1,28 +1,30 @@
+
 <?php
+
+echo '<h2> Soal Menentukan Katagori Nilai<h2>';
+
 function tentukan_nilai($number)
-{
-    //menampilkan grade berdasarkan hasil nilai akhir
 
-if ($number>=90)
+  //menampilkan grade berdasarkan hasil nilai akhir
 {
-$grade = “Sangat baik”;
-}
-elseif ($number>=75)
-{
-$grade = “Baik”;
-}
-elseif ($number>=60)
-{
-$grade = “Cukup”;
-}
-else
-{
-$grade = “Kurang”;
-}
+    if ($number>=85 & $number<100) { 
+        $nilai_akhir =  'Sangat baik'; 
 
+    }elseif ($number>=70 & $number<85) {
+        $nilai_akhir = 'Baik';
+
+    }elseif ($number>=60 & $number<70) {
+        $nilai_akhir = 'Cukup';
+    }else {
+        $nilai_akhir = 'Kurang';
+       
+    return $nilai_akhir; 
+    }  
 //TEST CASES
-echo tentukan_nilai(98);//Sangat Baik
-echo tentukan_nilai(76); //Baik
-echo tentukan_nilai(67); //Cukup
-echo tentukan_nilai(43); //Kurang
+
+echo tentukan_nilai(60); //Sangat Baik
+//echo tentukan_nilai(76); //Baik
+//echo tentukan_nilai(67); //Cukup
+//echo tentukan_nilai(43); //Kurang
+}
 ?>
